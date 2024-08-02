135ced985bd82622c4279601b76666757dba0401
nirmezamer <nirmezamer@mail.tau.ac.il>
2024-08-02 13:35:00 +0300

    create a full jobs API in backend

--- backend/module/jobs.py
+++ backend/module/jobs.py
@@ -12,8 +12,10 @@ def InsertJob(req: func.HttpRequest) -> func.HttpResponse:
     
     connection_string = os.getenv("AzureWebJobsStorage")
     try:
-        with TableClient.from_connection_string(connection_string, table_name="countertable") as table:
+        with TableClient.from_connection_string(connection_string, table_name="jobs") as table:
             req_body = req.get_json()
+            req_body["PartitionKey"] = req_body["Owner"]
+            req_body["RowKey"] = f"{req_body['Date']}_{req_body['Time']}"
             table.upsert_entity(entity=req_body, mode="merge")
             return func.HttpResponse(f"Job inserted successfully:\n {req_body}", status_code=200)
     except:
@@ -25,8 +27,10 @@ def RemoveJob(req: func.HttpRequest) -> func.HttpResponse:
     
     connection_string = os.getenv("AzureWebJobsStorage")
     try:
-        with TableClient.from_connection_string(connection_string, table_name="countertable") as table:
+        with TableClient.from_connection_string(connection_string, table_name="jobs") as table:
             req_body = req.get_json()
+            req_body["PartitionKey"] = req_body["Owner"]
+            req_body["RowKey"] = f"{req_body['Date']}_{req_body['Time']}"
             table.delete_entity(partition_key=req_body["PartitionKey"], row_key=req_body["RowKey"])
             return func.HttpResponse(f"Job deleted successfully:\n {req_body}", status_code=200)
     except:
@@ -38,11 +42,36 @@ def GetAllJobs(req: func.HttpRequest) -> func.HttpResponse:
     
     connection_string = os.getenv("AzureWebJobsStorage")
     try:
-        with TableClient.from_connection_string(connection_string, table_name="countertable") as table:
+        with TableClient.from_connection_string(connection_string, table_name="jobs") as table:
             entities = table.list_entities()
             response = []
             for entity in entities:
+                entity.pop("PartitionKey")
+                entity.pop("RowKey")
                 response.append(entity)
             return func.HttpResponse(json.dumps(response), status_code=200)
     except:
+        return func.HttpResponse("Error", status_code=500)
+
+@jobs.route(route="GetAllJobs/{owner}", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])    
+def GetAllJobsOfOwner(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('GetAllJobsOfOwner function processed a request.')
+    logging.info(f"Owner: {req.route_params['owner']}")
+    
+    _owner = req.route_params['owner'].replace('_', ' ')
+    
+    connection_string = os.getenv("AzureWebJobsStorage")
+    try:
+        with TableClient.from_connection_string(connection_string, table_name="jobs") as table:
+            logging.info(f"start query for Owner: {_owner}")
+            entities = table.query_entities(query_filter=f"PartitionKey eq '{_owner}'")
+            logging.info(f"end query for Owner: {_owner}")
+            response = []
+            for entity in entities:
+                entity.pop("PartitionKey")
+                entity.pop("RowKey")
+                response.append(entity)
+            return func.HttpResponse(json.dumps(response), status_code=200)
+    except Exception as e:
+        logging.info(f"Error: {e}")
         return func.HttpResponse("Error", status_code=500)
