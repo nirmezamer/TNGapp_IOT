cafc57259c22f9181fd4bf7f9f231a65a044425b
nirmezamer <nirmezamer@mail.tau.ac.il>
2024-10-04 20:59:43 +0300

    change signalR resource

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const config = {
-    local: false, // Set to true for local development, false for production
+    local: true, // Set to true for local development, false for production
     getBaseUrl() {
       return this.local ? "http://localhost:7071" : "https://tngapp1.azurewebsites.net";
     },
